b92ffe59f48d5bda7c72ed98d8f3e3f7f715b17d
thilina <thilinaudayanga1010@gmail.com>
2025-08-10 01:33:02 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
+
